67ad75434c8f122952c9585aa5a1a9334b5161cd
Wes Weitzel <wesweitzel@gmail.com>
2021-11-20 23:01:05 -0500

    ssl test

--- knexfile.js
+++ knexfile.js
@@ -34,7 +34,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL + '?ssl=true',
     migrations: {
       directory: './db/migrations',
     },
