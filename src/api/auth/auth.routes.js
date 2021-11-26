847727e30c8d50cb7e515f696b947b83fc05d3e4
Wes Weitzel <wesweitzel@gmail.com>
2021-11-26 14:13:27 -0500

    test

--- src/api/auth/auth.routes.js
+++ src/api/auth/auth.routes.js
@@ -42,14 +42,14 @@ router.get('/authenticate', async (req, res) => {
     }
 
     res.cookie('twitch_access_token', access_token, {
-      httpOnly: true,
+      httpOnly: false,
       secure: false,
       signed: false,
       sameSite: false
     });
 
     res.cookie('twitch_refresh_token', refresh_token, {
-      httpOnly: true,
+      httpOnly: false,
       secure: false,
       signed: false,
       sameSite: false
