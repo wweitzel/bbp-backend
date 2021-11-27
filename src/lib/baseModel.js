efcdfa0b0839b2a62f355a5f0d2c5bd3087a8438
Wes Weitzel <wesweitzel@gmail.com>
2021-11-27 16:20:07 -0500

    fix created_at issue with inserts

--- src/lib/baseModel.js
+++ src/lib/baseModel.js
@@ -2,11 +2,11 @@ const { Model } = require('objection');
 
 class BaseModel extends Model {
   $beforeInsert() {
-    this.created_at = new Date().toISOString();
+    this.createdAt = new Date().toISOString();
   }
 
   $beforeUpdate() {
-    this.updated_at = new Date().toISOString();
+    this.updatedAt = new Date().toISOString();
   }
 }
 
