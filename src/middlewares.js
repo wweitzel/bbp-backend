63021325bbb8932d8012b0037db88833b3209bc7
Wes Weitzel <wesweitzel@gmail.com>
2021-12-01 22:59:25 -0500

    log errors

--- src/middlewares.js
+++ src/middlewares.js
@@ -17,6 +17,7 @@ function notFound(req, res, next) {
 
 /* eslint-disable no-unused-vars */
 function errorHandler(err, req, res, next) {
+  console.error(err);
   /* eslint-enable no-unused-vars */
   const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
   res.status(statusCode);
